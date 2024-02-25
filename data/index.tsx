d628b05b5ab1558614aa8abe0d364c6db6f7f139
KT 2022 <103476893+kt946@users.noreply.github.com>
2024-02-25 17:40:13 -0500

    Add lesson 3 data

--- data/index.tsx
+++ data/index.tsx
@@ -1,8 +1,9 @@
 import lesson_0_data from './lessons/lesson-0/lesson-0-data';
 import lesson_1_data from './lessons/lesson-1/lesson-1-data';
 import lesson_2_data from './lessons/lesson-2/lesson-2-data';
+import lesson_3_data from './lessons/lesson-3/lesson-3-data';
 
-const genki_1_data = [lesson_0_data, lesson_1_data, lesson_2_data];
+const genki_1_data = [lesson_0_data, lesson_1_data, lesson_2_data, lesson_3_data];
 
 const genki_2_data = [lesson_0_data];
 
---
+++ data/lessons/lesson-3/lesson-3-data.tsx
@@ -0,0 +1,374 @@
+const lesson_3_data = {
+  lessonNumber: 3,
+  lessonTitle: 'Making a Date',
+  lessonPages: 'Genki I: p.82-101',
+  slug: 'lesson-3',
+  units: [
+    {
+      title: 'Vocabulary',
+      slug: 'vocabulary',
+      items: [
+        {
+          frontSide: 'えいが',
+          backSide: 'movie',
+        },
+        {
+          frontSide: 'おんがく',
+          backSide: 'music',
+        },
+        {
+          frontSide: 'ざっし',
+          backSide: 'magazine',
+        },
+        {
+          frontSide: 'スポーツ',
+          backSide: 'sports',
+        },
+        {
+          frontSide: 'デート',
+          backSide: 'date (romantic, not calendar)',
+        },
+        {
+          frontSide: 'テニス',
+          backSide: 'tennis',
+        },
+        {
+          frontSide: 'テレビ',
+          backSide: 'TV',
+        },
+        {
+          frontSide: 'アイスクリーム',
+          backSide: 'ice cream',
+        },
+        {
+          frontSide: 'ハンバーガー',
+          backSide: 'hamburger',
+        },
+        {
+          frontSide: 'おさけ',
+          backSide: 'sake; alcoholic drink',
+        },
+        {
+          frontSide: 'おちゃ',
+          backSide: 'green tea',
+        },
+        {
+          frontSide: 'コーヒー',
+          backSide: 'coffee',
+        },
+        {
+          frontSide: 'みず',
+          backSide: 'water',
+        },
+        {
+          frontSide: 'あさごはん',
+          backSide: 'breakfast',
+        },
+        {
+          frontSide: 'ひるごはん',
+          backSide: 'lunch',
+        },
+        {
+          frontSide: 'ばんごはん',
+          backSide: 'dinner',
+        },
+        {
+          frontSide: 'いえ',
+          backSide: 'home; house',
+        },
+        {
+          frontSide: 'うち',
+          backSide: 'home; house; my place',
+        },
+        {
+          frontSide: 'がっこう',
+          backSide: 'school',
+        },
+        {
+          frontSide: 'カフェ',
+          backSide: 'cafe',
+        },
+        {
+          frontSide: 'あした',
+          backSide: 'tomorrow',
+        },
+        {
+          frontSide: 'きょう',
+          backSide: 'today',
+        },
+        {
+          frontSide: 'あさ',
+          backSide: 'morning',
+        },
+        {
+          frontSide: 'こんばん',
+          backSide: 'tonight',
+        },
+        {
+          frontSide: 'まいにち',
+          backSide: 'every day',
+        },
+        {
+          frontSide: 'まいばん',
+          backSide: 'every night',
+        },
+        {
+          frontSide: 'しゅうまつ',
+          backSide: 'weekend',
+        },
+        {
+          frontSide: 'どようび',
+          backSide: 'Saturday',
+        },
+        {
+          frontSide: 'にちようび',
+          backSide: 'Sunday',
+        },
+        {
+          frontSide: 'いつ',
+          backSide: 'when',
+        },
+        {
+          frontSide: '～ごろ',
+          backSide: 'at about ...',
+        },
+        {
+          frontSide: 'いく',
+          backSide: 'to go',
+        },
+        {
+          frontSide: 'かえる',
+          backSide: 'to go back; to return',
+        },
+        {
+          frontSide: 'きく',
+          backSide: 'to listen; to hear',
+        },
+        {
+          frontSide: 'のむ',
+          backSide: 'to drink',
+        },
+        {
+          frontSide: 'はなす',
+          backSide: 'to speak; to talk',
+        },
+        {
+          frontSide: 'よむ',
+          backSide: 'to read',
+        },
+        {
+          frontSide: 'おきる',
+          backSide: 'to get up',
+        },
+        {
+          frontSide: 'たべる',
+          backSide: 'to eat',
+        },
+        {
+          frontSide: 'ねる',
+          backSide: 'to sleep; to go to sleep',
+        },
+        {
+          frontSide: 'みる',
+          backSide: 'to see; to look at; to watch',
+        },
+        {
+          frontSide: 'くる',
+          backSide: 'to come',
+        },
+        {
+          frontSide: 'する',
+          backSide: 'to do',
+        },
+        {
+          frontSide: 'べんきょうする',
+          backSide: 'to study',
+        },
+        {
+          frontSide: 'いい',
+          backSide: 'good',
+        },
+        {
+          frontSide: 'はやい',
+          backSide: 'early',
+        },
+        {
+          frontSide: 'あまり + negative',
+          backSide: 'not much',
+        },
+        {
+          frontSide: 'ぜんぜん + negative',
+          backSide: 'not at all',
+        },
+        {
+          frontSide: 'たいてい',
+          backSide: 'usually',
+        },
+        {
+          frontSide: 'ちょっと',
+          backSide: 'a little',
+        },
+        {
+          frontSide: 'ときどき',
+          backSide: 'sometimes',
+        },
+        {
+          frontSide: 'よく',
+          backSide: 'often; much',
+        },
+        {
+          frontSide: 'そうですね',
+          backSide: "That's right.; Let me see.",
+        },
+        {
+          frontSide: 'でも',
+          backSide: 'but',
+        },
+        {
+          frontSide: 'どうですか',
+          backSide: 'How about ...?; How is ...?',
+        },
+        {
+          frontSide: 'ええ',
+          backSide: 'yes',
+        },
+      ],
+    },
+    {
+      title: 'Kanji Readings',
+      slug: 'kanji-readings',
+      items: [
+        {
+          frontSide: '一',
+          backSide: 'いち',
+        },
+        {
+          frontSide: '一時',
+          backSide: 'いちじ',
+        },
+        {
+          frontSide: '二',
+          backSide: 'に',
+        },
+        {
+          frontSide: '二時',
+          backSide: 'にじ',
+        },
+        {
+          frontSide: '三',
+          backSide: 'さん',
+        },
+        {
+          frontSide: '三時',
+          backSide: 'さんじ',
+        },
+        {
+          frontSide: '四',
+          backSide: 'よん',
+        },
+        {
+          frontSide: '四時',
+          backSide: 'よじ',
+        },
+        {
+          frontSide: '五',
+          backSide: 'ご',
+        },
+        {
+          frontSide: '五時',
+          backSide: 'ごじ',
+        },
+        {
+          frontSide: '六',
+          backSide: 'ろく',
+        },
+        {
+          frontSide: '六時',
+          backSide: 'ろくじ',
+        },
+        {
+          frontSide: '六百',
+          backSide: 'ろっぴゃく',
+        },
+        {
+          frontSide: '七',
+          backSide: 'しち／なな',
+        },
+        {
+          frontSide: '七時',
+          backSide: 'しちじ',
+        },
+        {
+          frontSide: '八',
+          backSide: 'はち',
+        },
+        {
+          frontSide: '八時',
+          backSide: 'はちじ',
+        },
+        {
+          frontSide: '八百',
+          backSide: 'はっぴゃく',
+        },
+        {
+          frontSide: '九',
+          backSide: 'きゅう',
+        },
+        {
+          frontSide: '九時',
+          backSide: 'くじ',
+        },
+        {
+          frontSide: '十',
+          backSide: 'じゅう',
+        },
+        {
+          frontSide: '十時',
+          backSide: 'じゅうじ',
+        },
+        {
+          frontSide: '百',
+          backSide: 'ひゃく',
+        },
+        {
+          frontSide: '三百',
+          backSide: 'さんびゃく',
+        },
+        {
+          frontSide: '千',
+          backSide: 'せん',
+        },
+        {
+          frontSide: '三千',
+          backSide: 'さんぜん',
+        },
+        {
+          frontSide: '八千',
+          backSide: 'はっせん',
+        },
+        {
+          frontSide: '千円',
+          backSide: 'せんえん',
+        },
+        {
+          frontSide: '一万',
+          backSide: 'いちまん',
+        },
+        {
+          frontSide: '十万',
+          backSide: 'じゅうまん',
+        },
+        {
+          frontSide: '百万',
+          backSide: 'ひゃくまん',
+        },
+        {
+          frontSide: '百円',
+          backSide: 'ひゃくえん',
+        },
+      ],
+    },
+  ],
+};
+
+export default lesson_3_data;
