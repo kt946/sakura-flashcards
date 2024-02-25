de8e85479914997afb7aef71f11e3ad0a251cb49
KT 2022 <103476893+kt946@users.noreply.github.com>
2024-02-25 17:47:56 -0500

    Add lesson 7 data

--- data/index.tsx
+++ data/index.tsx
@@ -5,8 +5,18 @@ import lesson_3_data from './lessons/lesson-3/lesson-3-data';
 import lesson_4_data from './lessons/lesson-4/lesson-4-data';
 import lesson_5_data from './lessons/lesson-5/lesson-5-data';
 import lesson_6_data from './lessons/lesson-6/lesson-6-data';
+import lesson_7_data from './lessons/lesson-7/lesson-7-data';
 
-const genki_1_data = [lesson_0_data, lesson_1_data, lesson_2_data, lesson_3_data, lesson_4_data, lesson_5_data, lesson_6_data];
+const genki_1_data = [
+  lesson_0_data,
+  lesson_1_data,
+  lesson_2_data,
+  lesson_3_data,
+  lesson_4_data,
+  lesson_5_data,
+  lesson_6_data,
+  lesson_7_data,
+];
 
 const genki_2_data = [lesson_0_data];
 
---
+++ data/lessons/lesson-7/lesson-7-data.tsx
@@ -0,0 +1,322 @@
+const lesson_7_data = {
+  lessonNumber: 7,
+  lessonTitle: 'Family Picture',
+  lessonPages: 'Genki I: p.166-185',
+  slug: 'lesson-7',
+  units: [
+    {
+      title: 'Vocabulary',
+      slug: 'vocabulary',
+      items: [
+        {
+          frontSide: 'かぞく',
+          backSide: 'family',
+        },
+        {
+          frontSide: 'おじいさん',
+          backSide: 'grandfather; old man',
+        },
+        {
+          frontSide: 'おばあさん',
+          backSide: 'grandmother; old woman',
+        },
+        {
+          frontSide: 'おにいさん',
+          backSide: 'older brother',
+        },
+        {
+          frontSide: 'おねえさん',
+          backSide: 'older sister',
+        },
+        {
+          frontSide: 'ちち',
+          backSide: '(my) father',
+        },
+        {
+          frontSide: 'はは',
+          backSide: '(my) mother',
+        },
+        {
+          frontSide: 'あに',
+          backSide: '(my) older brother',
+        },
+        {
+          frontSide: 'あね',
+          backSide: '(my) older sister',
+        },
+        {
+          frontSide: 'いもうと',
+          backSide: 'younger sister',
+        },
+        {
+          frontSide: 'おとうと',
+          backSide: 'younger brother',
+        },
+        {
+          frontSide: 'きょうだい',
+          backSide: 'brothers and sisters',
+        },
+        {
+          frontSide: 'おとこのひと',
+          backSide: 'man',
+        },
+        {
+          frontSide: 'おんなのひと',
+          backSide: 'woman',
+        },
+        {
+          frontSide: 'かいしゃ',
+          backSide: 'company',
+        },
+        {
+          frontSide: 'しょくどう',
+          backSide: 'cafeteria; dining commons',
+        },
+        {
+          frontSide: 'デパート',
+          backSide: 'department store',
+        },
+        {
+          frontSide: 'かみ',
+          backSide: 'hair',
+        },
+        {
+          frontSide: 'くち',
+          backSide: 'mouth',
+        },
+        {
+          frontSide: 'め',
+          backSide: 'eye',
+        },
+        {
+          frontSide: 'めがね',
+          backSide: 'glasses',
+        },
+        {
+          frontSide: 'うた',
+          backSide: 'song',
+        },
+        {
+          frontSide: 'サークル',
+          backSide: 'club activity',
+        },
+        {
+          frontSide: 'くるま',
+          backSide: 'car',
+        },
+        {
+          frontSide: 'ながい',
+          backSide: 'long',
+        },
+        {
+          frontSide: 'みじかい',
+          backSide: 'short (length)',
+        },
+        {
+          frontSide: 'はやい',
+          backSide: 'fast',
+        },
+        {
+          frontSide: 'せがたかい',
+          backSide: 'tall (stature)',
+        },
+        {
+          frontSide: 'せがひくい',
+          backSide: 'short (stature)',
+        },
+        {
+          frontSide: 'あたまがいい',
+          backSide: 'bright; smart; clever',
+        },
+        {
+          frontSide: 'かわいい',
+          backSide: 'cute',
+        },
+        {
+          frontSide: 'しんせつ（な）',
+          backSide: 'kind',
+        },
+        {
+          frontSide: 'べんり（な）',
+          backSide: 'convenient',
+        },
+        {
+          frontSide: 'うたう',
+          backSide: 'to sing',
+        },
+        {
+          frontSide: 'かぶる',
+          backSide: 'to put on (a hat)',
+        },
+        {
+          frontSide: 'はく',
+          backSide: 'to put on (items below your waist)',
+        },
+        {
+          frontSide: 'しる',
+          backSide: 'to get to know',
+        },
+        {
+          frontSide: 'しっています',
+          backSide: 'I know',
+        },
+        {
+          frontSide: 'しりません',
+          backSide: 'I do not know',
+        },
+        {
+          frontSide: 'すむ',
+          backSide: 'to live',
+        },
+        {
+          frontSide: 'はたらく',
+          backSide: 'to work',
+        },
+        {
+          frontSide: 'ふとる',
+          backSide: 'to gain weight',
+        },
+        {
+          frontSide: 'ふとっています',
+          backSide: 'to be on the heavy side; overweight',
+        },
+        {
+          frontSide: '（めがねを）かける',
+          backSide: 'to put on (glasses)',
+        },
+        {
+          frontSide: 'きる',
+          backSide: 'to put on (clothes above your waist)',
+        },
+        {
+          frontSide: 'やせる',
+          backSide: 'to lose weight',
+        },
+        {
+          frontSide: 'やせています',
+          backSide: 'to be thin',
+        },
+        {
+          frontSide: 'けっこんする',
+          backSide: 'to get married',
+        },
+        {
+          frontSide: '～が',
+          backSide: '... , but',
+        },
+        {
+          frontSide: 'なにも + negative',
+          backSide: 'not ... anything',
+        },
+        {
+          frontSide: '～にん',
+          backSide: '(counter for people)',
+        },
+        {
+          frontSide: 'ひとり',
+          backSide: 'one person',
+        },
+        {
+          frontSide: 'ふたり',
+          backSide: 'two people',
+        },
+        {
+          frontSide: 'べつに + negative',
+          backSide: 'nothing in particular',
+        },
+        {
+          frontSide: 'もちろん',
+          backSide: 'of course',
+        },
+        {
+          frontSide: 'よかったら',
+          backSide: 'if you like',
+        },
+      ],
+    },
+    {
+      title: 'Kanji Readings',
+      slug: 'kanji-readings',
+      items: [
+        {
+          frontSide: '東京',
+          backSide: 'とうきょう',
+        },
+        {
+          frontSide: '京子',
+          backSide: 'きょうこ',
+        },
+        {
+          frontSide: '子ども',
+          backSide: 'こども',
+        },
+        {
+          frontSide: '小さい',
+          backSide: 'ちいさい',
+        },
+        {
+          frontSide: '会う',
+          backSide: 'あう',
+        },
+        {
+          frontSide: '会社',
+          backSide: 'かいしゃ',
+        },
+        {
+          frontSide: '父',
+          backSide: 'ちち',
+        },
+        {
+          frontSide: 'お父さん',
+          backSide: 'おとうさん',
+        },
+        {
+          frontSide: '母',
+          backSide: 'はは',
+        },
+        {
+          frontSide: 'お母さん',
+          backSide: 'おかあさん',
+        },
+        {
+          frontSide: '高い',
+          backSide: 'たかい',
+        },
+        {
+          frontSide: '高校',
+          backSide: 'こうこう',
+        },
+        {
+          frontSide: '高校生',
+          backSide: 'こうこうせい',
+        },
+        {
+          frontSide: '学校',
+          backSide: 'がっこう',
+        },
+        {
+          frontSide: '毎日',
+          backSide: 'まいにち',
+        },
+        {
+          frontSide: '日本語',
+          backSide: 'にほんご',
+        },
+        {
+          frontSide: '文学',
+          backSide: 'ぶんがく',
+        },
+        {
+          frontSide: '帰る',
+          backSide: 'かえる',
+        },
+        {
+          frontSide: '入る',
+          backSide: 'はいる',
+        },
+      ],
+    },
+  ],
+};
+
+export default lesson_7_data;
