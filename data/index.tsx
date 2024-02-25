022bb5b702886c51481eefce5426469c8699879d
KT 2022 <103476893+kt946@users.noreply.github.com>
2024-02-25 17:42:19 -0500

    Add lesson 4 data

--- data/index.tsx
+++ data/index.tsx
@@ -2,8 +2,9 @@ import lesson_0_data from './lessons/lesson-0/lesson-0-data';
 import lesson_1_data from './lessons/lesson-1/lesson-1-data';
 import lesson_2_data from './lessons/lesson-2/lesson-2-data';
 import lesson_3_data from './lessons/lesson-3/lesson-3-data';
+import lesson_4_data from './lessons/lesson-4/lesson-4-data';
 
-const genki_1_data = [lesson_0_data, lesson_1_data, lesson_2_data, lesson_3_data];
+const genki_1_data = [lesson_0_data, lesson_1_data, lesson_2_data, lesson_3_data, lesson_4_data];
 
 const genki_2_data = [lesson_0_data];
 
---
+++ data/lessons/lesson-4/lesson-4-data.tsx
@@ -0,0 +1,342 @@
+const lesson_4_data = {
+  lessonNumber: 4,
+  lessonTitle: 'The First Date',
+  lessonPages: 'Genki I: p.102-127',
+  slug: 'lesson-4',
+  units: [
+    {
+      title: 'Vocabulary',
+      slug: 'vocabulary',
+      items: [
+        {
+          frontSide: 'ゲーム',
+          backSide: 'game',
+        },
+        {
+          frontSide: 'アルバイト',
+          backSide: 'part-time job',
+        },
+        {
+          frontSide: 'かいもの',
+          backSide: 'shopping',
+        },
+        {
+          frontSide: 'クラス',
+          backSide: 'class',
+        },
+        {
+          frontSide: 'いぬ',
+          backSide: 'dog',
+        },
+        {
+          frontSide: 'ねこ',
+          backSide: 'cat',
+        },
+        {
+          frontSide: 'ひと',
+          backSide: 'person',
+        },
+        {
+          frontSide: 'こども',
+          backSide: 'child',
+        },
+        {
+          frontSide: 'あなた',
+          backSide: 'you',
+        },
+        {
+          frontSide: 'いす',
+          backSide: 'chair',
+        },
+        {
+          frontSide: 'つくえ',
+          backSide: 'desk',
+        },
+        {
+          frontSide: 'しゃしん',
+          backSide: 'picture; photograph',
+        },
+        {
+          frontSide: 'はな',
+          backSide: 'flower',
+        },
+        {
+          frontSide: 'レポート',
+          backSide: '(term) paper',
+        },
+        {
+          frontSide: 'ごはん',
+          backSide: 'rice; meal',
+        },
+        {
+          frontSide: 'パン',
+          backSide: 'bread',
+        },
+        {
+          frontSide: 'おてら',
+          backSide: 'temple',
+        },
+        {
+          frontSide: 'こうえん',
+          backSide: 'park',
+        },
+        {
+          frontSide: 'スーパー',
+          backSide: 'supermarket',
+        },
+        {
+          frontSide: 'バスてい',
+          backSide: 'bus stop',
+        },
+        {
+          frontSide: 'びょういん',
+          backSide: 'hospital',
+        },
+        {
+          frontSide: 'ホテル',
+          backSide: 'hotel',
+        },
+        {
+          frontSide: 'ほんや',
+          backSide: 'bookstore',
+        },
+        {
+          frontSide: 'まち',
+          backSide: 'town; city',
+        },
+        {
+          frontSide: 'レストラン',
+          backSide: 'restaurant',
+        },
+        {
+          frontSide: 'きのう',
+          backSide: 'yesterday',
+        },
+        {
+          frontSide: '～じかん',
+          backSide: '... hours',
+        },
+        {
+          frontSide: 'いちじかん',
+          backSide: 'one hour',
+        },
+        {
+          frontSide: 'せんしゅう',
+          backSide: 'last week',
+        },
+        {
+          frontSide: 'とき',
+          backSide: 'when ...; at the time of ...',
+        },
+        {
+          frontSide: 'げつようび',
+          backSide: 'Monday',
+        },
+        {
+          frontSide: 'かようび',
+          backSide: 'Tuesday',
+        },
+        {
+          frontSide: 'すいようび',
+          backSide: 'Wednesday',
+        },
+        {
+          frontSide: 'もくようび',
+          backSide: 'Thursday',
+        },
+        {
+          frontSide: 'きんようび',
+          backSide: 'Friday',
+        },
+        {
+          frontSide: 'あう',
+          backSide: 'to meet; to see (a person)',
+        },
+        {
+          frontSide: 'ある',
+          backSide: 'there is ...',
+        },
+        {
+          frontSide: 'かう',
+          backSide: 'to buy',
+        },
+        {
+          frontSide: 'かく',
+          backSide: 'to write',
+        },
+        {
+          frontSide: 'とる',
+          backSide: 'to take (a picture)',
+        },
+        {
+          frontSide: 'まつ',
+          backSide: 'to wait',
+        },
+        {
+          frontSide: 'わかる',
+          backSide: 'to understand',
+        },
+        {
+          frontSide: 'いる',
+          backSide: '(a person) is in .. ; stays at ...',
+        },
+        {
+          frontSide: '～ぐらい',
+          backSide: 'about (approximate measurement)',
+        },
+        {
+          frontSide: 'ごめんなさい',
+          backSide: "I'm sorry.",
+        },
+        {
+          frontSide: 'それから',
+          backSide: 'and then',
+        },
+        {
+          frontSide: 'だから',
+          backSide: 'so; therefore',
+        },
+        {
+          frontSide: 'たくさん',
+          backSide: 'many; a lot',
+        },
+        {
+          frontSide: '～と',
+          backSide: 'together with (a person); and',
+        },
+        {
+          frontSide: 'どうして',
+          backSide: 'why',
+        },
+        {
+          frontSide: 'ひとりで',
+          backSide: 'alone',
+        },
+        {
+          frontSide: 'もしもし',
+          backSide: 'Hello? (used on the phone)',
+        },
+        {
+          frontSide: 'みぎ',
+          backSide: 'right (～の)',
+        },
+        {
+          frontSide: 'ひだり',
+          backSide: 'left (～の)',
+        },
+        {
+          frontSide: 'まえ',
+          backSide: 'front (～の)',
+        },
+        {
+          frontSide: 'うしろ',
+          backSide: 'back (～の)',
+        },
+        {
+          frontSide: 'なか',
+          backSide: 'inside (～の)',
+        },
+        {
+          frontSide: 'うえ',
+          backSide: 'on (～の)',
+        },
+        {
+          frontSide: 'した',
+          backSide: 'under (～の)',
+        },
+        {
+          frontSide: 'ちかく',
+          backSide: 'near; nearby (～の)',
+        },
+        {
+          frontSide: 'となり',
+          backSide: 'next (～の)',
+        },
+        {
+          frontSide: 'あいだ',
+          backSide: 'between (A と B の)',
+        },
+      ],
+    },
+    {
+      title: 'Kanji Readings',
+      slug: 'kanji-readings',
+      items: [
+        {
+          frontSide: '日本',
+          backSide: 'にほん',
+        },
+        {
+          frontSide: '日曜日',
+          backSide: 'にちようび',
+        },
+        {
+          frontSide: '本',
+          backSide: 'ほん',
+        },
+        {
+          frontSide: '日本人',
+          backSide: 'にほんじん',
+        },
+        {
+          frontSide: '一人で',
+          backSide: 'ひとりで',
+        },
+        {
+          frontSide: 'この人',
+          backSide: 'このひと',
+        },
+        {
+          frontSide: '月曜日',
+          backSide: 'げつようび',
+        },
+        {
+          frontSide: '一月',
+          backSide: 'いちがつ',
+        },
+        {
+          frontSide: '火曜日',
+          backSide: 'かようび',
+        },
+        {
+          frontSide: '水曜日',
+          backSide: 'すいようび',
+        },
+        {
+          frontSide: '水',
+          backSide: 'みず/すい',
+        },
+        {
+          frontSide: '木曜日',
+          backSide: 'もくようび',
+        },
+        {
+          frontSide: '金曜日',
+          backSide: 'きんようび',
+        },
+        {
+          frontSide: '土曜日',
+          backSide: 'どようび',
+        },
+        {
+          frontSide: '上',
+          backSide: 'うえ',
+        },
+        {
+          frontSide: '下',
+          backSide: 'した',
+        },
+        {
+          frontSide: '中',
+          backSide: 'なか',
+        },
+        {
+          frontSide: '三時半',
+          backSide: 'さんじはん',
+        },
+      ],
+    },
+  ],
+};
+
+export default lesson_4_data;
