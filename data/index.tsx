456c8a5cd14d16c5ab3749456774f1d826f7727e
KT 2022 <103476893+kt946@users.noreply.github.com>
2024-02-25 17:38:02 -0500

    Add lesson 2 data

--- data/index.tsx
+++ data/index.tsx
@@ -1,7 +1,8 @@
 import lesson_0_data from './lessons/lesson-0/lesson-0-data';
 import lesson_1_data from './lessons/lesson-1/lesson-1-data';
+import lesson_2_data from './lessons/lesson-2/lesson-2-data';
 
-const genki_1_data = [lesson_0_data, lesson_1_data];
+const genki_1_data = [lesson_0_data, lesson_1_data, lesson_2_data];
 
 const genki_2_data = [lesson_0_data];
 
---
+++ data/lessons/lesson-2/lesson-2-data.tsx
@@ -0,0 +1,440 @@
+const lesson_2_data = {
+  lessonNumber: 2,
+  lessonTitle: 'Shopping',
+  lessonPages: 'Genki I: p.56-81',
+  slug: 'lesson-2',
+  units: [
+    {
+      title: 'Vocabulary',
+      slug: 'vocabulary',
+      items: [
+        {
+          frontSide: 'これ',
+          backSide: 'this one',
+        },
+        {
+          frontSide: 'それ',
+          backSide: 'that one',
+        },
+        {
+          frontSide: 'あれ',
+          backSide: 'that one (over there)',
+        },
+        {
+          frontSide: 'どれ',
+          backSide: 'which one',
+        },
+        {
+          frontSide: 'この',
+          backSide: 'this ...',
+        },
+        {
+          frontSide: 'その',
+          backSide: 'that ...',
+        },
+        {
+          frontSide: 'あの',
+          backSide: 'that ... (over there)',
+        },
+        {
+          frontSide: 'どの',
+          backSide: 'which ...',
+        },
+        {
+          frontSide: 'ここ',
+          backSide: 'here',
+        },
+        {
+          frontSide: 'そこ',
+          backSide: 'there',
+        },
+        {
+          frontSide: 'あそこ',
+          backSide: 'over there',
+        },
+        {
+          frontSide: 'どこ',
+          backSide: 'where',
+        },
+        {
+          frontSide: 'だれ',
+          backSide: 'who',
+        },
+        {
+          frontSide: 'おいしい',
+          backSide: 'delicious',
+        },
+        {
+          frontSide: 'さかな',
+          backSide: 'fish',
+        },
+        {
+          frontSide: 'とんかつ',
+          backSide: 'pork cutlet',
+        },
+        {
+          frontSide: 'にく',
+          backSide: 'meat',
+        },
+        {
+          frontSide: 'メニュー',
+          backSide: 'menu',
+        },
+        {
+          frontSide: 'やさい',
+          backSide: 'vegetable',
+        },
+        {
+          frontSide: 'かさ',
+          backSide: 'umbrella',
+        },
+        {
+          frontSide: 'かばん',
+          backSide: 'bag',
+        },
+        {
+          frontSide: 'くつ',
+          backSide: 'shoes',
+        },
+        {
+          frontSide: 'さいふ',
+          backSide: 'wallet',
+        },
+        {
+          frontSide: 'ジーンズ',
+          backSide: 'jeans',
+        },
+        {
+          frontSide: 'じてんしゃ',
+          backSide: 'bicycle',
+        },
+        {
+          frontSide: 'しんぶん',
+          backSide: 'newspaper',
+        },
+        {
+          frontSide: 'スマホ',
+          backSide: 'smartphone; mobile',
+        },
+        {
+          frontSide: 'Ｔシャツ',
+          backSide: 'T-shirt',
+        },
+        {
+          frontSide: 'とけい',
+          backSide: 'watch; clock',
+        },
+        {
+          frontSide: 'ノート',
+          backSide: 'notebook',
+        },
+        {
+          frontSide: 'ペン',
+          backSide: 'pen',
+        },
+        {
+          frontSide: 'ぼうし',
+          backSide: 'hat; cap',
+        },
+        {
+          frontSide: 'ほん',
+          backSide: 'book',
+        },
+        {
+          frontSide: 'ぎんこう',
+          backSide: 'bank',
+        },
+        {
+          frontSide: 'コンビニ',
+          backSide: 'convenience store',
+        },
+        {
+          frontSide: 'トイレ',
+          backSide: 'toilet; restroom',
+        },
+        {
+          frontSide: 'としょかん',
+          backSide: 'library',
+        },
+        {
+          frontSide: 'ゆうびんきょく',
+          backSide: 'post office',
+        },
+        {
+          frontSide: 'イギリス',
+          backSide: 'Britain',
+        },
+        {
+          frontSide: 'かんこく',
+          backSide: 'Korea',
+        },
+        {
+          frontSide: 'ちゅうごく',
+          backSide: 'China',
+        },
+        {
+          frontSide: 'えいご',
+          backSide: 'English (language)',
+        },
+        {
+          frontSide: 'けいざい',
+          backSide: 'economics',
+        },
+        {
+          frontSide: 'コンピューター',
+          backSide: 'computer',
+        },
+        {
+          frontSide: 'ビジネス',
+          backSide: 'business',
+        },
+        {
+          frontSide: 'れきし',
+          backSide: 'history',
+        },
+        {
+          frontSide: 'おかあさん',
+          backSide: 'mother',
+        },
+        {
+          frontSide: 'おとうさん',
+          backSide: 'father',
+        },
+        {
+          frontSide: 'いくら',
+          backSide: 'how much',
+        },
+        {
+          frontSide: '～えん',
+          backSide: '... yen',
+        },
+        {
+          frontSide: 'たかい',
+          backSide: 'expensive; high',
+        },
+        {
+          frontSide: 'いらっしゃいませ',
+          backSide: 'Welcome (to our store).',
+        },
+        {
+          frontSide: '(～を) おねがいします',
+          backSide: '..., please.',
+        },
+        {
+          frontSide: '(～を)ください',
+          backSide: 'Please give me ...',
+        },
+        {
+          frontSide: 'じゃあ',
+          backSide: 'then ...; if that is the case, ...',
+        },
+        {
+          frontSide: 'どうぞ',
+          backSide: 'Please.; Here it is.',
+        },
+        {
+          frontSide: 'どうも',
+          backSide: 'Thank you.',
+        },
+      ],
+    },
+    {
+      title: 'Numbers',
+      slug: 'numbers',
+      items: [
+        {
+          frontSide: 'ひゃく',
+          backSide: '100',
+        },
+        {
+          frontSide: 'にひゃく',
+          backSide: '200',
+        },
+        {
+          frontSide: 'さんびゃく',
+          backSide: '300',
+        },
+        {
+          frontSide: 'よんひゃく',
+          backSide: '400',
+        },
+        {
+          frontSide: 'ごひゃく',
+          backSide: '500',
+        },
+        {
+          frontSide: 'ろっぴゃく',
+          backSide: '600',
+        },
+        {
+          frontSide: 'ななひゃく',
+          backSide: '700',
+        },
+        {
+          frontSide: 'はっぴゃく',
+          backSide: '800',
+        },
+        {
+          frontSide: 'きゅうひゃく',
+          backSide: '900',
+        },
+        {
+          frontSide: 'せん',
+          backSide: '1,000',
+        },
+        {
+          frontSide: 'にせん',
+          backSide: '2,000',
+        },
+        {
+          frontSide: 'さんぜん',
+          backSide: '3,000',
+        },
+        {
+          frontSide: 'よんせん',
+          backSide: '4,000',
+        },
+        {
+          frontSide: 'ごせん',
+          backSide: '5,000',
+        },
+        {
+          frontSide: 'ろくせん',
+          backSide: '6,000',
+        },
+        {
+          frontSide: 'ななせん',
+          backSide: '7,000',
+        },
+        {
+          frontSide: 'はっせん',
+          backSide: '8,000',
+        },
+        {
+          frontSide: 'きゅうせん',
+          backSide: '9,000',
+        },
+        {
+          frontSide: 'いちまん',
+          backSide: '10,000',
+        },
+        {
+          frontSide: 'にまん',
+          backSide: '20,000',
+        },
+        {
+          frontSide: 'さんまん',
+          backSide: '30,000',
+        },
+        {
+          frontSide: 'よんまん',
+          backSide: '40,000',
+        },
+        {
+          frontSide: 'ごまん',
+          backSide: '50,000',
+        },
+        {
+          frontSide: 'ろくまん',
+          backSide: '60,000',
+        },
+        {
+          frontSide: 'ななまん',
+          backSide: '70,000',
+        },
+        {
+          frontSide: 'はちまん',
+          backSide: '80,000',
+        },
+        {
+          frontSide: 'きゅうまん',
+          backSide: '90,000',
+        },
+      ],
+    },
+    {
+      title: 'Useful Expressions (In the Classroom)',
+      slug: 'useful-expressions',
+      items: [
+        {
+          frontSide: 'こくばん',
+          backSide: 'blackboard',
+        },
+        {
+          frontSide: 'でんき',
+          backSide: 'light',
+        },
+        {
+          frontSide: 'カーテン',
+          backSide: 'curtain',
+        },
+        {
+          frontSide: 'つくえ',
+          backSide: 'desk',
+        },
+        {
+          frontSide: 'けしゴム',
+          backSide: 'eraser',
+        },
+        {
+          frontSide: 'じしょ',
+          backSide: 'dictionary',
+        },
+        {
+          frontSide: 'えんぴつ',
+          backSide: 'pencil',
+        },
+        {
+          frontSide: 'ドア',
+          backSide: 'door',
+        },
+        {
+          frontSide: 'ほん',
+          backSide: 'book',
+        },
+        {
+          frontSide: 'まど',
+          backSide: 'window',
+        },
+        {
+          frontSide: 'いす',
+          backSide: 'chair',
+        },
+        {
+          frontSide: 'わかりましたか。',
+          backSide: 'Do you understand?',
+        },
+        {
+          frontSide: 'わかりました。',
+          backSide: 'I understand./I understood.',
+        },
+        {
+          frontSide: 'わかりません',
+          backSide: "I don't understand./I don't know.",
+        },
+        {
+          frontSide: 'ゆっくりいってください。',
+          backSide: 'Please say it slowly.',
+        },
+        {
+          frontSide: 'もういちどいってください。',
+          backSide: 'Please say it again.',
+        },
+        {
+          frontSide: 'ちょっとまってください。',
+          backSide: 'Please wait for a while.',
+        },
+        {
+          frontSide: 'きいてください。',
+          backSide: 'Please listen./Please ask.',
+        },
+        {
+          frontSide: '10ページをみてください。',
+          backSide: 'Please look at page 10.',
+        },
+      ],
+    },
+  ],
+};
+
+export default lesson_2_data;
