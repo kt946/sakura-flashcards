6cb4ba7ba8e7fe142f1be6e3012561f7aa750ba6
KT 2022 <103476893+kt946@users.noreply.github.com>
2024-02-25 17:45:46 -0500

    Add lesson 6 data

--- data/index.tsx
+++ data/index.tsx
@@ -4,8 +4,9 @@ import lesson_2_data from './lessons/lesson-2/lesson-2-data';
 import lesson_3_data from './lessons/lesson-3/lesson-3-data';
 import lesson_4_data from './lessons/lesson-4/lesson-4-data';
 import lesson_5_data from './lessons/lesson-5/lesson-5-data';
+import lesson_6_data from './lessons/lesson-6/lesson-6-data';
 
-const genki_1_data = [lesson_0_data, lesson_1_data, lesson_2_data, lesson_3_data, lesson_4_data, lesson_5_data];
+const genki_1_data = [lesson_0_data, lesson_1_data, lesson_2_data, lesson_3_data, lesson_4_data, lesson_5_data, lesson_6_data];
 
 const genki_2_data = [lesson_0_data];
 
---
+++ data/lessons/lesson-6/lesson-6-data.tsx
@@ -0,0 +1,302 @@
+const lesson_6_data = {
+  lessonNumber: 6,
+  lessonTitle: "A Day in Robert's Life",
+  lessonPages: 'Genki I: p.146-165',
+  slug: 'lesson-6',
+  units: [
+    {
+      title: 'Vocabulary',
+      slug: 'vocabulary',
+      items: [
+        {
+          frontSide: 'かんじ',
+          backSide: 'kanji; Chinese character',
+        },
+        {
+          frontSide: 'きょうかしょ',
+          backSide: 'textbook',
+        },
+        {
+          frontSide: 'ページ',
+          backSide: 'page',
+        },
+        {
+          frontSide: 'つぎ',
+          backSide: 'next',
+        },
+        {
+          frontSide: 'おかね',
+          backSide: 'money',
+        },
+        {
+          frontSide: 'にもつ',
+          backSide: 'baggage',
+        },
+        {
+          frontSide: 'パソコン',
+          backSide: 'personal computer',
+        },
+        {
+          frontSide: 'シャワー',
+          backSide: 'shower',
+        },
+        {
+          frontSide: 'エアコン',
+          backSide: 'air conditioner',
+        },
+        {
+          frontSide: 'でんき',
+          backSide: 'electricity; light',
+        },
+        {
+          frontSide: 'まど',
+          backSide: 'window',
+        },
+        {
+          frontSide: 'でんしゃ',
+          backSide: 'train',
+        },
+        {
+          frontSide: 'くに',
+          backSide: 'country; place of origin',
+        },
+        {
+          frontSide: 'こんしゅう',
+          backSide: 'this week',
+        },
+        {
+          frontSide: 'らいしゅう',
+          backSide: 'next week',
+        },
+        {
+          frontSide: 'らいねん',
+          backSide: 'next year',
+        },
+        {
+          frontSide: 'よる',
+          backSide: 'night',
+        },
+        {
+          frontSide: 'たいへん（な）',
+          backSide: 'tough (situation)',
+        },
+        {
+          frontSide: 'あそぶ',
+          backSide: 'to play; to spend time pleasantly',
+        },
+        {
+          frontSide: 'いそぐ',
+          backSide: 'to hurry',
+        },
+        {
+          frontSide: 'かえす',
+          backSide: 'to return (a thing)',
+        },
+        {
+          frontSide: 'けす',
+          backSide: 'to turn off; to erase',
+        },
+        {
+          frontSide: 'しぬ',
+          backSide: 'to die',
+        },
+        {
+          frontSide: 'すわる',
+          backSide: 'to sit down',
+        },
+        {
+          frontSide: 'たつ',
+          backSide: 'to stand up',
+        },
+        {
+          frontSide: 'たばこをすう',
+          backSide: 'to smoke',
+        },
+        {
+          frontSide: 'つかう',
+          backSide: 'to use',
+        },
+        {
+          frontSide: 'てつだう',
+          backSide: 'to help',
+        },
+        {
+          frontSide: 'はいる',
+          backSide: 'to enter',
+        },
+        {
+          frontSide: 'もつ',
+          backSide: 'to carry; to hold',
+        },
+        {
+          frontSide: 'やすむ',
+          backSide: 'to be absent (from ... ); to rest',
+        },
+        {
+          frontSide: 'あける',
+          backSide: 'to open (something)',
+        },
+        {
+          frontSide: 'しめる',
+          backSide: 'to close (something)',
+        },
+        {
+          frontSide: 'おしえる',
+          backSide: 'to teach; to instruct',
+        },
+        {
+          frontSide: 'わすれる',
+          backSide: 'to forget; to leave behind',
+        },
+        {
+          frontSide: 'おりる',
+          backSide: 'to get off',
+        },
+        {
+          frontSide: 'かりる',
+          backSide: 'to borrow',
+        },
+        {
+          frontSide: 'シャワーをあびる',
+          backSide: 'to take a shower',
+        },
+        {
+          frontSide: 'つける',
+          backSide: 'to turn on',
+        },
+        {
+          frontSide: 'でんわする',
+          backSide: 'to call',
+        },
+        {
+          frontSide: 'つれてくる',
+          backSide: 'to bring (a person)',
+        },
+        {
+          frontSide: 'もってくる',
+          backSide: 'to bring (a thing)',
+        },
+        {
+          frontSide: 'あとで',
+          backSide: 'later on',
+        },
+        {
+          frontSide: 'すぐ',
+          backSide: 'right away',
+        },
+        {
+          frontSide: 'ゆっくり',
+          backSide: 'slowly; leisurely; unhurriedly',
+        },
+        {
+          frontSide: 'けっこうです',
+          backSide: "That would be fine.; That wouldn't be necessary.",
+        },
+        {
+          frontSide: 'ほんとうですか',
+          backSide: 'Really?',
+        },
+      ],
+    },
+    {
+      title: 'Kanji Readings',
+      slug: 'kanji-readings',
+      items: [
+        {
+          frontSide: '東',
+          backSide: 'ひがし',
+        },
+        {
+          frontSide: '東口',
+          backSide: 'ひがしぐち',
+        },
+        {
+          frontSide: '西',
+          backSide: 'にし',
+        },
+        {
+          frontSide: '西口',
+          backSide: 'にしぐち',
+        },
+        {
+          frontSide: '南',
+          backSide: 'みなみ',
+        },
+        {
+          frontSide: '南口',
+          backSide: 'みなみぐち',
+        },
+        {
+          frontSide: '北',
+          backSide: 'きた',
+        },
+        {
+          frontSide: '北口',
+          backSide: 'きたぐち',
+        },
+        {
+          frontSide: '出る',
+          backSide: 'でる',
+        },
+        {
+          frontSide: '出口',
+          backSide: 'でぐち',
+        },
+        {
+          frontSide: '出かける',
+          backSide: 'でかける',
+        },
+        {
+          frontSide: '右',
+          backSide: 'みぎ',
+        },
+        {
+          frontSide: '左',
+          backSide: 'ひだり',
+        },
+        {
+          frontSide: '五分',
+          backSide: 'ごふん',
+        },
+        {
+          frontSide: '十分',
+          backSide: 'じゅっぷん／じっぷん',
+        },
+        {
+          frontSide: '先生',
+          backSide: 'せんせい',
+        },
+        {
+          frontSide: '学生',
+          backSide: 'がくせい',
+        },
+        {
+          frontSide: '大学生',
+          backSide: 'だいがくせい',
+        },
+        {
+          frontSide: '大きい',
+          backSide: 'おおきい',
+        },
+        {
+          frontSide: '大学',
+          backSide: 'だいがく',
+        },
+        {
+          frontSide: '外国',
+          backSide: 'がいこく',
+        },
+        {
+          frontSide: '外国人',
+          backSide: 'がいこくじん',
+        },
+        {
+          frontSide: '中国',
+          backSide: 'ちゅうごく',
+        },
+      ],
+    },
+  ],
+};
+
+export default lesson_6_data;
