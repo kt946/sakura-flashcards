0a041e1192c4abba59a186f0f6be2eccad0f74c6
KT 2022 <103476893+kt946@users.noreply.github.com>
2024-02-25 17:44:04 -0500

    Add lesson 5 data

---
+++ data/lessons/lesson-5/lesson-5-data.tsx
@@ -0,0 +1,294 @@
+const lesson_5_data = {
+  lessonNumber: 5,
+  lessonTitle: 'A Trip to Okinawa',
+  lessonPages: 'Genki I: p.128-145',
+  slug: 'lesson-5',
+  units: [
+    {
+      title: 'Vocabulary',
+      slug: 'vocabulary',
+      items: [
+        {
+          frontSide: 'たべもの',
+          backSide: 'food',
+        },
+        {
+          frontSide: 'のみもの',
+          backSide: 'drink',
+        },
+        {
+          frontSide: 'くだもの',
+          backSide: 'fruit',
+        },
+        {
+          frontSide: 'やすみ',
+          backSide: 'holiday; day off; absence',
+        },
+        {
+          frontSide: 'りょこう',
+          backSide: 'travel',
+        },
+        {
+          frontSide: 'うみ',
+          backSide: 'sea',
+        },
+        {
+          frontSide: 'サーフィン',
+          backSide: 'surfing',
+        },
+        {
+          frontSide: 'おみやげ',
+          backSide: 'souvenir',
+        },
+        {
+          frontSide: 'バス',
+          backSide: 'bus',
+        },
+        {
+          frontSide: 'てんき',
+          backSide: 'weather',
+        },
+        {
+          frontSide: 'しゅくだい',
+          backSide: 'homework',
+        },
+        {
+          frontSide: 'テスト',
+          backSide: 'test',
+        },
+        {
+          frontSide: 'たんじょうび',
+          backSide: 'birthday',
+        },
+        {
+          frontSide: 'へや',
+          backSide: 'room',
+        },
+        {
+          frontSide: 'ぼく',
+          backSide: 'I (used by men)',
+        },
+        {
+          frontSide: 'Ｌサイズ（エルサイズ）',
+          backSide: 'size L',
+        },
+        {
+          frontSide: 'あたらしい',
+          backSide: 'new',
+        },
+        {
+          frontSide: 'ふるい',
+          backSide: 'old (thing)',
+        },
+        {
+          frontSide: 'あつい',
+          backSide: 'hot (weather)',
+        },
+        {
+          frontSide: 'さむい',
+          backSide: 'cold (weather)',
+        },
+        {
+          frontSide: 'あつい',
+          backSide: 'hot (thing)',
+        },
+        {
+          frontSide: 'いそがしい',
+          backSide: 'busy (people/days)',
+        },
+        {
+          frontSide: 'おおきい',
+          backSide: 'large',
+        },
+        {
+          frontSide: 'ちいさい',
+          backSide: 'small',
+        },
+        {
+          frontSide: 'おもしろい',
+          backSide: 'interesting; funny',
+        },
+        {
+          frontSide: 'つまらない',
+          backSide: 'boring',
+        },
+        {
+          frontSide: 'やさしい',
+          backSide: 'easy (problem); kind (person)',
+        },
+        {
+          frontSide: 'むずかしい',
+          backSide: 'difficult',
+        },
+        {
+          frontSide: 'かっこいい',
+          backSide: 'good-looking',
+        },
+        {
+          frontSide: 'こわい',
+          backSide: 'frightening',
+        },
+        {
+          frontSide: 'たのしい',
+          backSide: 'fun',
+        },
+        {
+          frontSide: 'やすい',
+          backSide: 'inexpensive; cheap (thing)',
+        },
+        {
+          frontSide: 'すき（な）',
+          backSide: 'fond of; to like',
+        },
+        {
+          frontSide: 'きらい（な）',
+          backSide: 'disgusted with; to dislike',
+        },
+        {
+          frontSide: 'だいすき（な）',
+          backSide: 'very fond of; to love',
+        },
+        {
+          frontSide: 'だいきらい（な）',
+          backSide: 'to hate',
+        },
+        {
+          frontSide: 'きれい（な）',
+          backSide: 'beautiful; clean',
+        },
+        {
+          frontSide: 'げんき（な）',
+          backSide: 'healthy; energetic',
+        },
+        {
+          frontSide: 'しずか（な）',
+          backSide: 'quiet',
+        },
+        {
+          frontSide: 'にぎやか（な）',
+          backSide: 'lively',
+        },
+        {
+          frontSide: 'ひま（な）',
+          backSide: 'not busy; free (time)',
+        },
+        {
+          frontSide: 'およぐ',
+          backSide: 'to swim',
+        },
+        {
+          frontSide: 'きく',
+          backSide: 'to ask',
+        },
+        {
+          frontSide: 'のる',
+          backSide: 'to ride; to board',
+        },
+        {
+          frontSide: 'やる',
+          backSide: 'to do; to perform',
+        },
+        {
+          frontSide: 'でかける',
+          backSide: 'to go out',
+        },
+        {
+          frontSide: 'いっしょに',
+          backSide: 'together',
+        },
+        {
+          frontSide: 'すごく',
+          backSide: 'extremely',
+        },
+        {
+          frontSide: 'だいじょうぶ',
+          backSide: "It's okay.; Not to worry.; Everything is under control.",
+        },
+        {
+          frontSide: 'とても',
+          backSide: 'very',
+        },
+        {
+          frontSide: 'どんな',
+          backSide: 'what kind of ...',
+        },
+        {
+          frontSide: '～まい',
+          backSide: '(counter for flat objects)',
+        },
+      ],
+    },
+    {
+      title: 'Kanji Readings',
+      slug: 'kanji-readings',
+      items: [
+        {
+          frontSide: '山',
+          backSide: 'やま',
+        },
+        {
+          frontSide: '山川さん',
+          backSide: 'やまかわさん',
+        },
+        {
+          frontSide: '川',
+          backSide: 'かわ',
+        },
+        {
+          frontSide: '元気な',
+          backSide: 'げんきな',
+        },
+        {
+          frontSide: '天気',
+          backSide: 'てんき',
+        },
+        {
+          frontSide: '私',
+          backSide: 'わたし',
+        },
+        {
+          frontSide: '今',
+          backSide: 'いま',
+        },
+        {
+          frontSide: '今日',
+          backSide: 'きょう',
+        },
+        {
+          frontSide: '田中さん',
+          backSide: 'たなかさん',
+        },
+        {
+          frontSide: '山田さん',
+          backSide: 'やまださん',
+        },
+        {
+          frontSide: '女の人',
+          backSide: 'おんなのひと',
+        },
+        {
+          frontSide: '男の人',
+          backSide: 'おとこのひと',
+        },
+        {
+          frontSide: '見る',
+          backSide: 'みる',
+        },
+        {
+          frontSide: '行く',
+          backSide: 'いく',
+        },
+        {
+          frontSide: '食べる',
+          backSide: 'たべる',
+        },
+        {
+          frontSide: '飲む',
+          backSide: 'のむ',
+        },
+      ],
+    },
+  ],
+};
+
+export default lesson_5_data;
