07a13c8a7d5bef2ec5b6d522874e01928da33eb1
KT 2022 <103476893+kt946@users.noreply.github.com>
2024-02-25 17:04:05 -0500

    Add Not Found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex items-center">
+        <h1 className="inline-block pr-6 mr-5 text-2xl font-medium leading-[49px] border-r border-neutral-600">404</h1>
+        <p className="text-sm">This page could not be found.</p>
+      </div>
+    </div>
+  );
+}
