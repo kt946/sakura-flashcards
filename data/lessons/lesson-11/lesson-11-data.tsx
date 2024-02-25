eb8f3f15c553653246b7360bfab11a6b38fcc614
KT 2022 <103476893+kt946@users.noreply.github.com>
2024-02-25 17:58:17 -0500

    Add lesson 11 data

---
+++ data/lessons/lesson-11/lesson-11-data.tsx
@@ -0,0 +1,342 @@
+const lesson_11_data = {
+  lessonNumber: 11,
+  lessonTitle: 'After the Vacation',
+  lessonPages: 'Genki I: p.254-271',
+  slug: 'lesson-11',
+  units: [
+    {
+      title: 'Vocabulary',
+      slug: 'vocabulary',
+      items: [
+        {
+          frontSide: 'がいこく',
+          backSide: 'foreign country',
+        },
+        {
+          frontSide: 'オーストラリア',
+          backSide: 'Australia',
+        },
+        {
+          frontSide: 'かわ',
+          backSide: 'river',
+        },
+        {
+          frontSide: 'おんせん',
+          backSide: 'spa; hot spring',
+        },
+        {
+          frontSide: 'つり',
+          backSide: 'fishing',
+        },
+        {
+          frontSide: 'みずうみ',
+          backSide: 'lake',
+        },
+        {
+          frontSide: 'やま',
+          backSide: 'mountain',
+        },
+        {
+          frontSide: 'キャンプ',
+          backSide: 'camp',
+        },
+        {
+          frontSide: 'ドライブ',
+          backSide: 'drive',
+        },
+        {
+          frontSide: 'じんじゃ',
+          backSide: 'shrine',
+        },
+        {
+          frontSide: 'びじゅつかん',
+          backSide: 'art museum',
+        },
+        {
+          frontSide: 'しゃちょう',
+          backSide: 'president of a company',
+        },
+        {
+          frontSide: 'かしゅ',
+          backSide: 'singer',
+        },
+        {
+          frontSide: 'ルームメイト',
+          backSide: 'roommate',
+        },
+        {
+          frontSide: 'ホストファミリー',
+          backSide: 'host family',
+        },
+        {
+          frontSide: 'しょうらい',
+          backSide: 'future',
+        },
+        {
+          frontSide: 'ゆめ',
+          backSide: 'dream',
+        },
+        {
+          frontSide: 'おまつり',
+          backSide: 'festival',
+        },
+        {
+          frontSide: 'おしょうがつ',
+          backSide: "New Year's",
+        },
+        {
+          frontSide: 'おかし',
+          backSide: 'snack; sweets',
+        },
+        {
+          frontSide: 'ビール',
+          backSide: 'beer',
+        },
+        {
+          frontSide: 'おもちゃ',
+          backSide: 'toy',
+        },
+        {
+          frontSide: 'こんがっき',
+          backSide: 'this semester',
+        },
+        {
+          frontSide: 'らいがっき',
+          backSide: 'next semester',
+        },
+        {
+          frontSide: 'じゅぎょう',
+          backSide: 'class',
+        },
+        {
+          frontSide: 'こちら',
+          backSide: 'this person (polite)',
+        },
+        {
+          frontSide: 'うそをつく',
+          backSide: 'to tell a lie',
+        },
+        {
+          frontSide: 'おなかがすく',
+          backSide: 'to become hungry',
+        },
+        {
+          frontSide: 'かう',
+          backSide: 'to own (a pet)',
+        },
+        {
+          frontSide: 'サボる',
+          backSide: 'to cut (classes)',
+        },
+        {
+          frontSide: 'とる',
+          backSide: 'to take (a class); to get (a grade)',
+        },
+        {
+          frontSide: 'ならう',
+          backSide: 'to learn',
+        },
+        {
+          frontSide: 'のぼる',
+          backSide: 'to climb',
+        },
+        {
+          frontSide: 'はしる',
+          backSide: 'to run',
+        },
+        {
+          frontSide: 'つかれる',
+          backSide: 'to get tired',
+        },
+        {
+          frontSide: 'やめる',
+          backSide: 'to quit',
+        },
+        {
+          frontSide: 'けんかする',
+          backSide: 'to have a fight; to quarrel',
+        },
+        {
+          frontSide: 'しょうかいする',
+          backSide: 'to introduce',
+        },
+        {
+          frontSide: 'ダイエットする',
+          backSide: 'to go on a diet',
+        },
+        {
+          frontSide: 'ちこくする',
+          backSide: 'to be late (for an appointment)',
+        },
+        {
+          frontSide: 'りゅうがく',
+          backSide: 'to study abroad',
+        },
+        {
+          frontSide: 'しゅっしん',
+          backSide: 'coming from',
+        },
+        {
+          frontSide: 'ひさしぶり',
+          backSide: 'it has been a long time',
+        },
+        {
+          frontSide: 'まあまあ',
+          backSide: 'okay; so-so',
+        },
+        {
+          frontSide: 'もっと',
+          backSide: 'more',
+        },
+        {
+          frontSide: 'あと',
+          backSide: 'after (an event)',
+        },
+        {
+          frontSide: 'そして',
+          backSide: 'and then',
+        },
+        {
+          frontSide: '～だけ',
+          backSide: 'just ... ; only ...',
+        },
+        {
+          frontSide: '～てん',
+          backSide: ' ... points',
+        },
+        {
+          frontSide: 'しょくぎょう',
+          backSide: 'occupation; business',
+        },
+        {
+          frontSide: 'さっか',
+          backSide: 'writer',
+        },
+        {
+          frontSide: 'ジャーナリスト',
+          backSide: 'journalist',
+        },
+        {
+          frontSide: 'けいさつかん',
+          backSide: 'police officer',
+        },
+        {
+          frontSide: 'しょうぼうし',
+          backSide: 'firefighter',
+        },
+        {
+          frontSide: 'きょうし',
+          backSide: 'school teacher',
+        },
+        {
+          frontSide: 'けんきゅうしゃ',
+          backSide: 'researcher',
+        },
+        {
+          frontSide: 'うちゅうひこうし',
+          backSide: 'astronaut',
+        },
+        {
+          frontSide: 'スポーツせんしゅ',
+          backSide: 'athlete',
+        },
+        {
+          frontSide: 'だいとうりょう',
+          backSide: 'president of a country',
+        },
+        {
+          frontSide: 'はいゆう',
+          backSide: 'actor; actress',
+        },
+        {
+          frontSide: 'かんごし',
+          backSide: 'nurse',
+        },
+        {
+          frontSide: 'シェフ',
+          backSide: 'chef',
+        },
+        {
+          frontSide: 'まんがか',
+          backSide: 'cartoonist',
+        },
+      ],
+    },
+    {
+      title: 'Kanji Readings',
+      slug: 'kanji-readings',
+      items: [
+        {
+          frontSide: '手紙',
+          backSide: 'てがみ',
+        },
+        {
+          frontSide: '歌手',
+          backSide: 'かしゅ',
+        },
+        {
+          frontSide: '好きな',
+          backSide: 'すきな',
+        },
+        {
+          frontSide: '大好きな',
+          backSide: 'だいすきな',
+        },
+        {
+          frontSide: '近く',
+          backSide: 'ちかく',
+        },
+        {
+          frontSide: '近所',
+          backSide: 'きんじょ',
+        },
+        {
+          frontSide: '明るい',
+          backSide: 'あかるい',
+        },
+        {
+          frontSide: '病院',
+          backSide: 'びょういん',
+        },
+        {
+          frontSide: '病気',
+          backSide: 'びょうき',
+        },
+        {
+          frontSide: '映画',
+          backSide: 'えいが',
+        },
+        {
+          frontSide: '歌う',
+          backSide: 'うたう',
+        },
+        {
+          frontSide: '歌',
+          backSide: 'うた',
+        },
+        {
+          frontSide: '川口市',
+          backSide: 'かわぐちし',
+        },
+        {
+          frontSide: 'いろいろな所',
+          backSide: 'いろいろなところ',
+        },
+        {
+          frontSide: '勉強する',
+          backSide: 'べんきょうする',
+        },
+        {
+          frontSide: '有名な',
+          backSide: 'ゆうめいな',
+        },
+        {
+          frontSide: '旅行',
+          backSide: 'りょこう',
+        },
+      ],
+    },
+  ],
+};
+
+export default lesson_11_data;
