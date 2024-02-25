8fbd10ff45330de74a57060b6155592d5018a62d
KT 2022 <103476893+kt946@users.noreply.github.com>
2024-02-25 17:59:39 -0500

    Add lesson 12 data

---
+++ data/lessons/lesson-12/lesson-12-data.tsx
@@ -0,0 +1,314 @@
+const lesson_12_data = {
+  lessonNumber: 12,
+  lessonTitle: 'Feeling Ill',
+  lessonPages: 'Genki I: p.272-294',
+  slug: 'lesson-12',
+  units: [
+    {
+      title: 'Vocabulary',
+      slug: 'vocabulary',
+      items: [
+        {
+          frontSide: 'おなか',
+          backSide: 'stomach',
+        },
+        {
+          frontSide: 'あし',
+          backSide: 'leg; foot',
+        },
+        {
+          frontSide: 'のど',
+          backSide: 'throat',
+        },
+        {
+          frontSide: 'は',
+          backSide: 'tooth',
+        },
+        {
+          frontSide: 'インフルエンザ',
+          backSide: 'influenza',
+        },
+        {
+          frontSide: 'かぜ',
+          backSide: 'cold',
+        },
+        {
+          frontSide: 'せき',
+          backSide: 'cough',
+        },
+        {
+          frontSide: 'ふつかよい',
+          backSide: 'hangover',
+        },
+        {
+          frontSide: 'ホームシック',
+          backSide: 'homesickness',
+        },
+        {
+          frontSide: 'アレルギー',
+          backSide: 'allergy',
+        },
+        {
+          frontSide: 'ジュース',
+          backSide: 'juice',
+        },
+        {
+          frontSide: 'たまご',
+          backSide: 'egg',
+        },
+        {
+          frontSide: 'ふく',
+          backSide: 'clothes',
+        },
+        {
+          frontSide: 'もの',
+          backSide: 'thing (concrete object)',
+        },
+        {
+          frontSide: 'プレゼント',
+          backSide: 'present',
+        },
+        {
+          frontSide: 'きっぷ',
+          backSide: '(train) ticket',
+        },
+        {
+          frontSide: '～だい',
+          backSide: 'charge; fee',
+        },
+        {
+          frontSide: 'ようじ',
+          backSide: 'business to take care of',
+        },
+        {
+          frontSide: 'おてあらい',
+          backSide: 'restroom',
+        },
+        {
+          frontSide: 'しあい',
+          backSide: 'match; game',
+        },
+        {
+          frontSide: 'せいじ',
+          backSide: 'politics',
+        },
+        {
+          frontSide: 'せいせき',
+          backSide: 'grade (on a test, etc.)',
+        },
+        {
+          frontSide: 'かのじょ',
+          backSide: 'she; girlfriend',
+        },
+        {
+          frontSide: 'かれ',
+          backSide: 'he; boyfriend',
+        },
+        {
+          frontSide: 'かれし',
+          backSide: 'boyfriend',
+        },
+        {
+          frontSide: 'いみ',
+          backSide: 'meaning',
+        },
+        {
+          frontSide: 'せまい',
+          backSide: 'narrow; not spacious',
+        },
+        {
+          frontSide: 'ひろい',
+          backSide: 'wide; spacious',
+        },
+        {
+          frontSide: 'わるい',
+          backSide: 'bad',
+        },
+        {
+          frontSide: 'いたい',
+          backSide: 'hurt; painful',
+        },
+        {
+          frontSide: 'あまい',
+          backSide: 'sweet',
+        },
+        {
+          frontSide: 'おおい',
+          backSide: 'there are many ...',
+        },
+        {
+          frontSide: 'すてき（な）',
+          backSide: 'nice',
+        },
+        {
+          frontSide: 'あるく',
+          backSide: 'to walk',
+        },
+        {
+          frontSide: 'かぜをひく',
+          backSide: 'to catch a cold',
+        },
+        {
+          frontSide: 'ねつがある',
+          backSide: 'to have a fever',
+        },
+        {
+          frontSide: 'のどがかわく',
+          backSide: 'to become thirsty',
+        },
+        {
+          frontSide: 'はらう',
+          backSide: 'to pay',
+        },
+        {
+          frontSide: 'なくす',
+          backSide: 'to lose',
+        },
+        {
+          frontSide: 'きょうみがある',
+          backSide: 'to be interested (in ... )',
+        },
+        {
+          frontSide: 'せきがでる',
+          backSide: 'to cough',
+        },
+        {
+          frontSide: 'わかれる',
+          backSide: 'to break up; to separate',
+        },
+        {
+          frontSide: 'きんちょうする',
+          backSide: 'to get nervous',
+        },
+        {
+          frontSide: 'しんぱいする',
+          backSide: 'to worry',
+        },
+        {
+          frontSide: 'おだいじに',
+          backSide: 'Get well soon.',
+        },
+        {
+          frontSide: 'げんきがない',
+          backSide: "don't look well",
+        },
+        {
+          frontSide: 'できるだけ',
+          backSide: 'as much as possible',
+        },
+        {
+          frontSide: 'たぶん',
+          backSide: 'probably; maybe',
+        },
+        {
+          frontSide: 'もうすぐ',
+          backSide: 'very soon; in a few moments/days',
+        },
+        {
+          frontSide: 'はじめて',
+          backSide: 'for the first time',
+        },
+        {
+          frontSide: 'にさんにち',
+          backSide: 'for two to three days',
+        },
+        {
+          frontSide: 'それに',
+          backSide: 'moreover, ...',
+        },
+        {
+          frontSide: 'おなじ',
+          backSide: 'same',
+        },
+      ],
+    },
+    {
+      title: 'Kanji Readings',
+      slug: 'kanji-readings',
+      items: [
+        {
+          frontSide: '昔',
+          backSide: 'むかし',
+        },
+        {
+          frontSide: '昔々',
+          backSide: 'むかしむかし',
+        },
+        {
+          frontSide: '人々',
+          backSide: 'ひとびと',
+        },
+        {
+          frontSide: '神さま',
+          backSide: 'かみさま',
+        },
+        {
+          frontSide: '神社',
+          backSide: 'じんじゃ',
+        },
+        {
+          frontSide: '早い',
+          backSide: 'はやい',
+        },
+        {
+          frontSide: '起きる',
+          backSide: 'おきる',
+        },
+        {
+          frontSide: '牛',
+          backSide: 'うし',
+        },
+        {
+          frontSide: '使う',
+          backSide: 'つかう',
+        },
+        {
+          frontSide: '働く',
+          backSide: 'はたらく',
+        },
+        {
+          frontSide: '連れて帰る',
+          backSide: 'つれてかえる',
+        },
+        {
+          frontSide: '別れる',
+          backSide: 'わかれる',
+        },
+        {
+          frontSide: '別に',
+          backSide: 'べつに',
+        },
+        {
+          frontSide: '一度',
+          backSide: 'いちど',
+        },
+        {
+          frontSide: '今度',
+          backSide: 'こんど',
+        },
+        {
+          frontSide: '赤',
+          backSide: 'あか',
+        },
+        {
+          frontSide: '赤い',
+          backSide: 'あかい',
+        },
+        {
+          frontSide: '青',
+          backSide: 'あお',
+        },
+        {
+          frontSide: '青い',
+          backSide: 'あおい',
+        },
+        {
+          frontSide: '色',
+          backSide: 'いろ',
+        },
+      ],
+    },
+  ],
+};
+
+export default lesson_12_data;
