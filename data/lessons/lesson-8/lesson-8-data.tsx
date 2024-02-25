62e9d72ddcb592b11e0ac35fd8e8e0f370c303ca
KT 2022 <103476893+kt946@users.noreply.github.com>
2024-02-25 17:49:25 -0500

    Add lesson 8 data

---
+++ data/lessons/lesson-8/lesson-8-data.tsx
@@ -0,0 +1,314 @@
+const lesson_8_data = {
+  lessonNumber: 8,
+  lessonTitle: 'Barbecue',
+  lessonPages: 'Genki I: p.186-209',
+  slug: 'lesson-8',
+  units: [
+    {
+      title: 'Vocabulary',
+      slug: 'vocabulary',
+      items: [
+        {
+          frontSide: 'はれ',
+          backSide: 'sunny weather',
+        },
+        {
+          frontSide: 'あめ',
+          backSide: 'rain',
+        },
+        {
+          frontSide: 'くもり',
+          backSide: 'cloudy weather',
+        },
+        {
+          frontSide: 'ゆき',
+          backSide: 'snow',
+        },
+        {
+          frontSide: 'てんきよほう',
+          backSide: 'weather forecast',
+        },
+        {
+          frontSide: 'きおん',
+          backSide: 'temperature (weather)',
+        },
+        {
+          frontSide: 'なつ',
+          backSide: 'summer',
+        },
+        {
+          frontSide: 'ふゆ',
+          backSide: 'winter',
+        },
+        {
+          frontSide: 'けさ',
+          backSide: 'this morning',
+        },
+        {
+          frontSide: 'あさって',
+          backSide: 'the day after tomorrow',
+        },
+        {
+          frontSide: 'まいしゅう',
+          backSide: 'every week',
+        },
+        {
+          frontSide: 'こんげつ',
+          backSide: 'this month',
+        },
+        {
+          frontSide: 'らいげつ',
+          backSide: 'next month',
+        },
+        {
+          frontSide: 'かいしゃいん',
+          backSide: 'office worker',
+        },
+        {
+          frontSide: 'しごと',
+          backSide: 'job; work; occupation',
+        },
+        {
+          frontSide: 'カメラ',
+          backSide: 'camera',
+        },
+        {
+          frontSide: 'カラオケ',
+          backSide: 'karaoke',
+        },
+        {
+          frontSide: 'ところ',
+          backSide: 'place',
+        },
+        {
+          frontSide: 'トマト',
+          backSide: 'tomato',
+        },
+        {
+          frontSide: 'はし',
+          backSide: 'chopsticks',
+        },
+        {
+          frontSide: 'パーティー',
+          backSide: 'party',
+        },
+        {
+          frontSide: 'バーベキュー',
+          backSide: 'barbecue',
+        },
+        {
+          frontSide: 'ホームステイ',
+          backSide: 'homestay; living with a local family',
+        },
+        {
+          frontSide: 'おふろ',
+          backSide: 'bath',
+        },
+        {
+          frontSide: 'スペイン',
+          backSide: 'Spain',
+        },
+        {
+          frontSide: 'なにか',
+          backSide: 'something',
+        },
+        {
+          frontSide: 'じょうず（な）',
+          backSide: 'skillful; good at ...',
+        },
+        {
+          frontSide: 'へた（な）',
+          backSide: 'clumsy; poor at ...',
+        },
+        {
+          frontSide: 'ゆうめい（な）',
+          backSide: 'famous',
+        },
+        {
+          frontSide: 'あらう',
+          backSide: 'to wash',
+        },
+        {
+          frontSide: 'いう',
+          backSide: 'to say',
+        },
+        {
+          frontSide: 'いる',
+          backSide: 'to need',
+        },
+        {
+          frontSide: 'おそくなる',
+          backSide: 'to be late',
+        },
+        {
+          frontSide: 'おふろにはいる',
+          backSide: 'to take a bath',
+        },
+        {
+          frontSide: 'おもう',
+          backSide: 'to think',
+        },
+        {
+          frontSide: 'きる',
+          backSide: 'to cut',
+        },
+        {
+          frontSide: 'つくる',
+          backSide: 'to make',
+        },
+        {
+          frontSide: '（あめ／ゆきが）ふる',
+          backSide: '(rain/snow) falls',
+        },
+        {
+          frontSide: 'もっていく',
+          backSide: 'to take (a thing)',
+        },
+        {
+          frontSide: 'すてる',
+          backSide: 'to throw away',
+        },
+        {
+          frontSide: 'はじめる',
+          backSide: 'to begin',
+        },
+        {
+          frontSide: 'うんてんする',
+          backSide: 'to drive',
+        },
+        {
+          frontSide: 'せんたくする',
+          backSide: 'to do laundry',
+        },
+        {
+          frontSide: 'そうじする',
+          backSide: 'to clean',
+        },
+        {
+          frontSide: 'りょうりする',
+          backSide: 'to cook',
+        },
+        {
+          frontSide: 'うん',
+          backSide: 'uh-huh; yes',
+        },
+        {
+          frontSide: 'ううん',
+          backSide: 'uh-uh; no',
+        },
+        {
+          frontSide: 'いつも',
+          backSide: 'always',
+        },
+        {
+          frontSide: 'おそく',
+          backSide: '(do something) late',
+        },
+        {
+          frontSide: 'かんぱい',
+          backSide: 'Cheers! (a toast)',
+        },
+        {
+          frontSide: 'みんなで',
+          backSide: 'all (of the people) together',
+        },
+        {
+          frontSide: 'ざんねん（ですね）',
+          backSide: "That's too bad.",
+        },
+        {
+          frontSide: 'まだ + negative',
+          backSide: 'not ... yet',
+        },
+        {
+          frontSide: '～ど',
+          backSide: '... degrees (temperature)',
+        },
+        {
+          frontSide: 'どう',
+          backSide: 'how',
+        },
+      ],
+    },
+    {
+      title: 'Kanji Readings',
+      slug: 'kanji-readings',
+      items: [
+        {
+          frontSide: '会社員',
+          backSide: 'かいしゃいん',
+        },
+        {
+          frontSide: '新しい',
+          backSide: 'あたらしい',
+        },
+        {
+          frontSide: '新聞',
+          backSide: 'しんぶん',
+        },
+        {
+          frontSide: '聞く',
+          backSide: 'きく',
+        },
+        {
+          frontSide: '作る',
+          backSide: 'つくる',
+        },
+        {
+          frontSide: '仕事',
+          backSide: 'しごと',
+        },
+        {
+          frontSide: '電車',
+          backSide: 'でんしゃ',
+        },
+        {
+          frontSide: '電気',
+          backSide: 'でんき',
+        },
+        {
+          frontSide: '車',
+          backSide: 'くるま',
+        },
+        {
+          frontSide: '休む',
+          backSide: 'やすむ',
+        },
+        {
+          frontSide: '休み',
+          backSide: 'やすみ',
+        },
+        {
+          frontSide: '言う',
+          backSide: 'いう',
+        },
+        {
+          frontSide: '読む',
+          backSide: 'よむ',
+        },
+        {
+          frontSide: '思う',
+          backSide: 'おもう',
+        },
+        {
+          frontSide: '次',
+          backSide: 'つぎ',
+        },
+        {
+          frontSide: '何',
+          backSide: 'なに',
+        },
+        {
+          frontSide: '何時',
+          backSide: 'なんじ',
+        },
+        {
+          frontSide: '何人',
+          backSide: 'なんにん',
+        },
+      ],
+    },
+  ],
+};
+
+export default lesson_8_data;
