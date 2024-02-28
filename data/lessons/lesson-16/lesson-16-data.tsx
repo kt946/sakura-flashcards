2d759a79d3f961347ba36c4a687236af1c0a86e4
KT 2022 <103476893+kt946@users.noreply.github.com>
2024-02-28 18:09:15 -0500

    Add lesson 16 data

---
+++ data/lessons/lesson-16/lesson-16-data.tsx
@@ -0,0 +1,302 @@
+const lesson_16_data = {
+  lessonNumber: 16,
+  lessonTitle: 'Lost and Found',
+  lessonPages: 'Genki II: p.92-113',
+  slug: 'lesson-16',
+  units: [
+    {
+      title: 'Vocabulary',
+      slug: 'vocabulary',
+      items: [
+        {
+          frontSide: 'えきいん（さん）',
+          backSide: 'station attendant',
+        },
+        {
+          frontSide: 'おや',
+          backSide: 'parent',
+        },
+        {
+          frontSide: 'しんせき',
+          backSide: 'relatives',
+        },
+        {
+          frontSide: 'ごみ',
+          backSide: 'garbage',
+        },
+        {
+          frontSide: 'さとう',
+          backSide: 'sugar',
+        },
+        {
+          frontSide: 'ファイル',
+          backSide: '(file) folder; portfolio; file',
+        },
+        {
+          frontSide: 'おおきさ',
+          backSide: 'size',
+        },
+        {
+          frontSide: 'みち',
+          backSide: 'way; road; directions',
+        },
+        {
+          frontSide: 'きまつしけん',
+          backSide: 'final examination',
+        },
+        {
+          frontSide: 'けんきゅう',
+          backSide: 'research',
+        },
+        {
+          frontSide: 'だいがくいん',
+          backSide: 'graduate school',
+        },
+        {
+          frontSide: 'しょうがくきん',
+          backSide: 'scholarship',
+        },
+        {
+          frontSide: 'すいせんじょう',
+          backSide: 'letter of recommendation',
+        },
+        {
+          frontSide: 'たいふう',
+          backSide: 'typhoon',
+        },
+        {
+          frontSide: 'ぶんか',
+          backSide: 'culture',
+        },
+        {
+          frontSide: 'へんじ',
+          backSide: 'reply',
+        },
+        {
+          frontSide: 'ひ',
+          backSide: 'day',
+        },
+        {
+          frontSide: 'きたない',
+          backSide: 'dirty',
+        },
+        {
+          frontSide: 'おこす',
+          backSide: 'to wake (someone) up',
+        },
+        {
+          frontSide: 'おごる',
+          backSide: 'to treat (someone) to a meal',
+        },
+        {
+          frontSide: 'わらう',
+          backSide: 'to laugh',
+        },
+        {
+          frontSide: 'おちこむ',
+          backSide: 'to get depressed',
+        },
+        {
+          frontSide: 'こまる',
+          backSide: 'to have difficulty',
+        },
+        {
+          frontSide: 'だす',
+          backSide: 'to take (something) out; to hand in (something)',
+        },
+        {
+          frontSide: 'なおす',
+          backSide: 'to correct; to fix',
+        },
+        {
+          frontSide: 'みつかる',
+          backSide: 'to be found',
+        },
+        {
+          frontSide: 'やくす',
+          backSide: 'to translate',
+        },
+        {
+          frontSide: 'かす',
+          backSide: 'to lend',
+        },
+        {
+          frontSide: 'つれていく',
+          backSide: 'to take (someone) to (a place)',
+        },
+        {
+          frontSide: 'みちにまよう',
+          backSide: "to become lost; to lose one's way",
+        },
+        {
+          frontSide: 'むかえにいく',
+          backSide: 'to go to pick up',
+        },
+        {
+          frontSide: 'あつめる',
+          backSide: 'to collect',
+        },
+        {
+          frontSide: 'いれる',
+          backSide: 'to put (something) in',
+        },
+        {
+          frontSide: 'みせる',
+          backSide: 'to show',
+        },
+        {
+          frontSide: 'のりおくれる',
+          backSide: 'to miss (a train, bus, etc.)',
+        },
+        {
+          frontSide: 'アイロンをかける',
+          backSide: 'to iron (clothes)',
+        },
+        {
+          frontSide: 'あさねぼうする',
+          backSide: 'to oversleep',
+        },
+        {
+          frontSide: 'あんないする',
+          backSide: 'to show (someone) around',
+        },
+        {
+          frontSide: 'せつめいする',
+          backSide: 'to explain',
+        },
+        {
+          frontSide: 'むかえにくる',
+          backSide: 'to come to pick up',
+        },
+        {
+          frontSide: 'きょうじゅうに',
+          backSide: 'by the end of today',
+        },
+        {
+          frontSide: 'じゅぎょうちゅうに',
+          backSide: 'in class; during the class',
+        },
+        {
+          frontSide: 'このあいだ',
+          backSide: 'the other day',
+        },
+        {
+          frontSide: 'これから',
+          backSide: 'from now on',
+        },
+        {
+          frontSide: 'このぐらい',
+          backSide: 'about this much',
+        },
+        {
+          frontSide: 'じぶんで',
+          backSide: '(do something) by oneself',
+        },
+        {
+          frontSide: 'ほかの',
+          backSide: 'other',
+        },
+        {
+          frontSide: 'ええと',
+          backSide: 'well . . . ; let me see . . .',
+        },
+        {
+          frontSide: 'じつは',
+          backSide: 'actually; in fact',
+        },
+        {
+          frontSide: '～いがい',
+          backSide: 'other than . . .',
+        },
+        {
+          frontSide: 'ごめん',
+          backSide: "I'm sorry. (casual)",
+        },
+        {
+          frontSide: 'しつれいします',
+          backSide: 'Excuse me.; Sorry to interrupt you.',
+        },
+      ],
+    },
+    {
+      title: 'Kanji Readings',
+      slug: 'kanji-readings',
+      items: [
+        {
+          frontSide: '子供',
+          backSide: 'こども',
+        },
+        {
+          frontSide: '世界',
+          backSide: 'せかい',
+        },
+        {
+          frontSide: '全部',
+          backSide: 'ぜんぶ',
+        },
+        {
+          frontSide: '部屋',
+          backSide: 'へや',
+        },
+        {
+          frontSide: '始まる',
+          backSide: 'はじまる',
+        },
+        {
+          frontSide: '始める',
+          backSide: 'はじめる',
+        },
+        {
+          frontSide: '毎週',
+          backSide: 'まいしゅう',
+        },
+        {
+          frontSide: '先週',
+          backSide: 'せんしゅう',
+        },
+        {
+          frontSide: '一週間',
+          backSide: 'いっしゅうかん',
+        },
+        {
+          frontSide: '考える',
+          backSide: 'かんがえる',
+        },
+        {
+          frontSide: '開ける',
+          backSide: 'あける',
+        },
+        {
+          frontSide: '本屋',
+          backSide: 'ほんや',
+        },
+        {
+          frontSide: '味方',
+          backSide: 'みかた',
+        },
+        {
+          frontSide: '運動',
+          backSide: 'うんどう',
+        },
+        {
+          frontSide: '運転',
+          backSide: 'うんてん',
+        },
+        {
+          frontSide: '教える',
+          backSide: 'おしえる',
+        },
+        {
+          frontSide: '教室',
+          backSide: 'きょうしつ',
+        },
+        {
+          frontSide: '以外',
+          backSide: 'いがい',
+        },
+      ],
+    },
+  ],
+};
+
+export default lesson_16_data;
