978b631ca93cf69433fe25e3493cd535c9de996e
KT 2022 <103476893+kt946@users.noreply.github.com>
2024-03-01 00:14:50 -0500

    Add lesson 18 data

---
+++ data/lessons/lesson-18/lesson-18-data.tsx
@@ -0,0 +1,302 @@
+const lesson_18_data = {
+  lessonNumber: 18,
+  lessonTitle: "John's Part-time Job",
+  lessonPages: 'Genki II: p.136-159',
+  slug: 'lesson-18',
+  units: [
+    {
+      title: 'Vocabulary',
+      slug: 'vocabulary',
+      items: [
+        {
+          frontSide: 'カーテン',
+          backSide: 'curtain',
+        },
+        {
+          frontSide: 'ソファ',
+          backSide: 'sofa',
+        },
+        {
+          frontSide: 'れいぞうこ',
+          backSide: 'refrigerator',
+        },
+        {
+          frontSide: 'スイッチ',
+          backSide: 'switch',
+        },
+        {
+          frontSide: 'しょうゆ',
+          backSide: 'soy sauce',
+        },
+        {
+          frontSide: 'スープ',
+          backSide: 'soup',
+        },
+        {
+          frontSide: 'バナナ',
+          backSide: 'banana',
+        },
+        {
+          frontSide: 'ポップコーン',
+          backSide: 'popcorn',
+        },
+        {
+          frontSide: 'シャンプー',
+          backSide: 'shampoo',
+        },
+        {
+          frontSide: 'ろうそく',
+          backSide: 'candle',
+        },
+        {
+          frontSide: 'タオル',
+          backSide: 'towel',
+        },
+        {
+          frontSide: 'スカート',
+          backSide: 'skirt',
+        },
+        {
+          frontSide: 'にっき',
+          backSide: 'diary',
+        },
+        {
+          frontSide: 'けいたい（でんわ）',
+          backSide: 'cell phone',
+        },
+        {
+          frontSide: 'さくら',
+          backSide: 'cherry blossom',
+        },
+        {
+          frontSide: 'むし',
+          backSide: 'insect',
+        },
+        {
+          frontSide: 'やちん',
+          backSide: 'rent',
+        },
+        {
+          frontSide: 'そと',
+          backSide: 'outside',
+        },
+        {
+          frontSide: 'あと',
+          backSide: 'the rest',
+        },
+        {
+          frontSide: 'ゆうがた',
+          backSide: 'evening',
+        },
+        {
+          frontSide: 'あかるい',
+          backSide: 'bright',
+        },
+        {
+          frontSide: 'くらい',
+          backSide: 'dark',
+        },
+        {
+          frontSide: 'はずかしい',
+          backSide: 'embarrassing; to feel embarrassed',
+        },
+        {
+          frontSide: 'たいせつ（な）',
+          backSide: 'precious; important',
+        },
+        {
+          frontSide: 'ふあん（な）',
+          backSide: 'anxious; worried',
+        },
+        {
+          frontSide: 'むり（な）',
+          backSide: 'impossible',
+        },
+        {
+          frontSide: 'あく',
+          backSide: '(something) opens',
+        },
+        {
+          frontSide: 'しまる',
+          backSide: '(something) closes',
+        },
+        {
+          frontSide: 'あやまる',
+          backSide: 'to apologize',
+        },
+        {
+          frontSide: 'おす',
+          backSide: 'to press; to push',
+        },
+        {
+          frontSide: 'おとす',
+          backSide: 'to drop (something)',
+        },
+        {
+          frontSide: 'おゆがわく',
+          backSide: 'water boils',
+        },
+        {
+          frontSide: 'ころぶ',
+          backSide: 'to fall down',
+        },
+        {
+          frontSide: 'こわす',
+          backSide: 'to break (something)',
+        },
+        {
+          frontSide: 'さく',
+          backSide: 'to bloom',
+        },
+        {
+          frontSide: 'たすかる',
+          backSide: 'to be saved; to be helped',
+        },
+        {
+          frontSide: 'たのむ',
+          backSide: 'to ask (a favor)',
+        },
+        {
+          frontSide: 'つく',
+          backSide: '(something) turns on',
+        },
+        {
+          frontSide: 'よごす',
+          backSide: 'to make dirty',
+        },
+        {
+          frontSide: 'おちる',
+          backSide: '(something) drops',
+        },
+        {
+          frontSide: 'かたづける',
+          backSide: 'to tidy up',
+        },
+        {
+          frontSide: 'かんがえる',
+          backSide: 'to think (about); to consider',
+        },
+        {
+          frontSide: 'きえる',
+          backSide: '(something) goes off',
+        },
+        {
+          frontSide: 'こわれる',
+          backSide: '(something) breaks',
+        },
+        {
+          frontSide: 'よごれる',
+          backSide: 'to become dirty',
+        },
+        {
+          frontSide: 'ちゅうもんする',
+          backSide: 'to place an order',
+        },
+        {
+          frontSide: 'いますぐ',
+          backSide: 'right away',
+        },
+        {
+          frontSide: '～までに',
+          backSide: 'by (time/date)',
+        },
+        {
+          frontSide: 'ほんとうに',
+          backSide: 'really',
+        },
+        {
+          frontSide: 'まず',
+          backSide: 'first of all',
+        },
+        {
+          frontSide: 'おかげで',
+          backSide: 'thanks to ...',
+        },
+        {
+          frontSide: 'どうしよう',
+          backSide: 'What should I/we do?',
+        },
+        {
+          frontSide: '～（ん）だろう',
+          backSide: 'short form of ～（ん）でしょう',
+        },
+        {
+          frontSide: 'おさきにしつれいします',
+          backSide: "See you. (lit., I'm leaving ahead of you.)",
+        },
+        {
+          frontSide: 'おつかれさま（でした）',
+          backSide: 'You must be tired after working so hard. (greeting between friends and coworkers)',
+        },
+      ],
+    },
+    {
+      title: 'Kanji Readings',
+      slug: 'kanji-readings',
+      items: [
+        {
+          frontSide: '目的',
+          backSide: 'もくてき',
+        },
+        {
+          frontSide: '目',
+          backSide: 'め',
+        },
+        {
+          frontSide: '洋服',
+          backSide: 'ようふく',
+        },
+        {
+          frontSide: '服',
+          backSide: 'ふく',
+        },
+        {
+          frontSide: '食堂',
+          backSide: 'しょくどう',
+        },
+        {
+          frontSide: '力仕事',
+          backSide: 'ちからしごと',
+        },
+        {
+          frontSide: '授業',
+          backSide: 'じゅぎょう',
+        },
+        {
+          frontSide: '試験',
+          backSide: 'しけん',
+        },
+        {
+          frontSide: '貸す',
+          backSide: 'かす',
+        },
+        {
+          frontSide: '図書館',
+          backSide: 'としょかん',
+        },
+        {
+          frontSide: '地図',
+          backSide: 'ちず',
+        },
+        {
+          frontSide: '旅館',
+          backSide: 'りょかん',
+        },
+        {
+          frontSide: '映画館',
+          backSide: 'えいがかん',
+        },
+        {
+          frontSide: '終わる',
+          backSide: 'おわる',
+        },
+        {
+          frontSide: '宿題',
+          backSide: 'しゅくだい',
+        },
+      ],
+    },
+  ],
+};
+
+export default lesson_18_data;
